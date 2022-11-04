bf72933c6d12e4567b502feb2b2fe9d7f74d07d3
Sebastian1047 <sebastiancastanedagarcia4@gmail.com>
2022-11-04 01:05:41 -0500

    Commit N 2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 //app.use('/usuarios',blogRoutes)
 
 app.get('/',(req,res)=>{
-    res.send('hola mundo')
+    res.send('hola mundo como anda')
 })
 app.listen(8000,console.log('servidor corriendo en http://localhost:3000/'))
 
