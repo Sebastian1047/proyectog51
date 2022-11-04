abf21726cfd296f5287f6fc5abe305d3686601cd
sebastian1047 <sebastiancastanedagarcia4@gmail.com>
2022-11-03 21:00:54 -0500

    Commit N 1

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from 'cors'
+
+//import blogRoutes from './routes/routes.js'
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+//app.use('/usuarios',blogRoutes)
+
+app.get('/',(req,res)=>{
+    res.send('hola mundo')
+})
+app.listen(8000,console.log('servidor corriendo en http://localhost:3000/'))
+
